a053ff73a7f948531c9a3427c307194cd1ac0934
Pasky <pasky@nomail.org>
2014-03-22 21:36:40 +0100

    misMacros: se añaden nuevas macros para elementos de formulario y una función mCC (miCaselCase)

--- app/misMacros.php
+++ app/misMacros.php
@@ -5,6 +5,60 @@
  * 
  */
 
+function mCC ($str, $suffix = null) {
+    return ucwords(str_replace('_', ' ', $str)) . $suffix;
+}
+
+
+HTML::macro('form_checkbox', function($name, $errors = null) {
+        $error = null;
+        if ($errors->first($name)) {
+                $error = '<div class="col-md-4 alert alert-danger">'. $errors->first($name) .'</div>';
+        }
+        return '
+<div class="form-group row">
+'. Form::label($name, mCC($name, ':'), array('class' => 'col-md-2 control-label')) .'
+    <div class="col-md-5">
+        '. Form::hidden($name, 0) .'
+        '. Form::checkbox($name, 1, array('class' => 'form-control input-md')) .'
+    </div>'.
+    $error .'
+</div>';
+});
+
+
+HTML::macro('form_select', function($name, $opts, $errors = null) {
+        $error = null;
+        if ($errors->first($name)) {
+                $error = '<div class="col-md-4 alert alert-danger">'. $errors->first($name) .'</div>';
+        }
+        return '
+<div class="form-group row">
+'. Form::label($name, mCC($name, ':'), array('class' => 'col-md-2 control-label')) .'
+    <div class="col-md-5">
+    '. Form::select($name, $opts, null, array('class' => 'form-control input-md')) .'
+    </div>'.
+    $error .'
+</div>';
+});
+
+
+HTML::macro('form_text', function($name, $errors = null) {
+        $error = null;
+        if ($errors->first($name)) {
+                $error = '<div class="col-md-4 alert alert-danger">'. $errors->first($name) .'</div>';
+        }
+        return '
+<div class="form-group row">
+'. Form::label($name, mCC($name, ':'), array('class' => 'col-md-2 control-label')) .'
+    <div class="col-md-5">
+        '. Form::text($name, null, array('class' => 'form-control input-md')) .'
+    </div>'.
+    $error .'
+</div>';
+});
+
+
 HTML::macro('nav_link', function($url, $text) {
     $class = ( Request::is($url) || Request::is($url.'/*') ) ? ' class="active"' : '';
     return '<li'.$class.'><a href="'.URL::to($url).'">'.$text.'</a></li>'. PHP_EOL;
